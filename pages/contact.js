51ed2c3732838311ddaac6e1e614392fde260ac9
Yana Krukovets <yanashelli@gmail.com>
2024-03-19 14:18:49 -0400

    contact img fix

--- pages/contact.js
+++ pages/contact.js
@@ -5,12 +5,14 @@ import en from "../locales/en";
 import fr from "../locales/fr";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { useMediaQuery } from "react-responsive";
 
 export default function Contact() {
   const router = useRouter();
 
   const { locale, asPath } = router;
   const t = locale === "en" ? en : fr;
+  const isMobile = useMediaQuery({ query: `(max-width: 760px)` });
 
   return (
     <>
@@ -67,14 +69,16 @@ export default function Contact() {
                 </div>
               </div>
 
-              <Image
-                src="/images/components/contact/art.webp"
-                alt="round art"
-                width={400}
-                height={450}
-                className="md:hidden object-cover"
-                priority={true}
-              />
+              {!isMobile && (
+                <Image
+                  src="/images/components/contact/art.webp"
+                  alt="round art"
+                  width={400}
+                  height={450}
+                  className="md:hidden object-cover"
+                  priority={true}
+                />
+              )}
             </div>
           </div>
         </div>
